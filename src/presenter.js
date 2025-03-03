5af85255cfe7f784e52e6b1a45bf373106678073
Isabella12-bit <isabella.montes@ucb.edu.bo>
2025-03-03 14:55:11 -0400

    Fix: Había un error que no mostraba el resultado y ya se lo arregló. También había otro error en el archivo .html ya que tanto multiplicacion como suma estaban en sumar-form

--- src/presenter.js
+++ src/presenter.js
@@ -2,16 +2,24 @@ import { sumar, multiplicar } from "./sumador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const form1 = document.querySelector("multiplicar-form");
+const formSumar = document.querySelector("#sumar-form");
+const formMultiplicar = document.querySelector("#multiplicar-form");
 const div = document.querySelector("#resultado-div");
 
-form.addEventListener("submit", (event) => {
+formSumar.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
-  div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML += "<p>Suma: " + sumar(firstNumber, secondNumber) + "</p>"; 
+});
+
+formMultiplicar.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div.innerHTML += "<p>Multiplicación: " + multiplicar(firstNumber, secondNumber) + "</p>";
 });
